2efc0475ebf47df284d982cf544adf15f03bf8c3
4hsnim <4hsnim@naver.com>
2022-07-26 00:52:53 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const cors = require('cors')
+const io = require('socket.io')(server,{
+    cors : {
+        origin :"*",
+        credentials :true
+    }
+});
+
+
+io.on('connection', socket=>{
+    socket.on('message',({name,message}) => {
+        io.emit('message',({name, message}))
+    })
+})
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+  });
+
+server.listen(4000, function(){
+    console.log('listening on port 4000');
+})
